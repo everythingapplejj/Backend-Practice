a896eea2ff6dc62349c121692a25df3c503a57ad
everythingapplejj <everythingapplejj@gmail.com>
2024-06-16 22:56:43 -0500

    restful API

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const app = require('express')(); // intialize 
+const express = require('express'); // Correctly require express
+const app = express(); // Initialize express
 
-const port = 8080
+const port = 8080;
 
+app.use(express.json())
+
+// Handle GET request for the root path
+app.get('/tshirt', (req, res) => {
+    res.status(200).send({
+        message: 'Hello World!',
+        price: 100
+    });
+    console.log("This is working");
+});
+
+app.post('/tshirt/:id', (req, res) => {
+    const {id} = req.params;
+    const {logo} = req.body;
+    if(!logo) {
+        res.status(418).send({message: 'We need a logo'});
+    }
+    res.send({
+        tshirt: `��� with your ${logo} and ID of ${id}`,
+    });
+});
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}); // this is a callback function 
+    console.log(`Server is running on http://localhost:${port}`);
+});
